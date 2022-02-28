4a02892514a4dd981b32b1d281a4af25e289ff33
IgorM07 <pythonbrother1331@gmail.com>
2022-03-01 00:37:14 +0200

    qq

---
+++ Tekwill lesson/Lesson 2/ex 3.py
@@ -0,0 +1,5 @@
+mystring = 'some string'
+
+some = mystring[0:4]
+string = mystring[5:]
+another_some = mystring[:4]
---
+++ Tekwill lesson/Lesson 3/ex 1.py
@@ -0,0 +1,4 @@
+aria_l = float(input('lenght - '))
+aria_L = float(input('weight - '))
+aria_total = (aria_l * aria_L)
+print('aria total - ' + str(aria_total))
